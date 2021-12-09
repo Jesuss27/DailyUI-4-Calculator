ac967932144b9e060c3e18c210cf5c4f0ea47123
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-12-09 14:11:04 -0800

    Add Logic to Calculator Buttons

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var state = "hello";
 const previousOperandTextElement = document.querySelector(`.previous-operand`)
 const currentOperandTextElement = document.querySelector(`.current-operand`)
+currentOperandTextElement.className = "current-operand"
 
 
 class Calculator {
@@ -18,23 +19,65 @@ class Calculator {
     }
 
     delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
 
     }
     appendNumber(number){
-        this.currentOperand = number
+        if(number =="." && this.currentOperand.includes(".")){
+            return
+        }
+        this.currentOperand = this.currentOperand.toString() + number.toString()
 
     }
 
     chooseOperation(operation){
+        if(this.currentOperand === "")return
+        if(this.previousOperand !== ""){
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = " "
+
 
     }
 
     compute(){
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return
+        switch (this.operation){
+            case "+" :
+                computation = prev + current
+                break
+                
+            case "-" :
+                computation = prev - current
+                break
+            case "x" :
+                computation = prev * current
+                break
+            case "÷" :
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ""
 
     }
 
     updateDisplay(){
         this.currentOperandTextElement.innerText = this.currentOperand
+        if(this.operation != undefined){
+        this.previousOperandTextElement.innerText = this.previousOperand + " " +this.operation
+        }else{
+            this.previousOperandTextElement.innerText = this.previousOperand
+        }
+    
 
     }
 
@@ -42,6 +85,55 @@ class Calculator {
 }
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+const operationButtons = document.querySelectorAll(`[data-operation]`)
+const equalsButton = document.querySelector("[data-equals]")
+const allClear = document.getElementById("all-clear")
+const deleteButton = document.getElementById("delete")
+
+
+
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", () =>{
+        console.log(button.innerHTML)
+        calculator.chooseOperation(button.innerHTML)
+        calculator.updateDisplay()
+        
+    })
+})
+
+equalsButton.addEventListener("click", () => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+equalsButton.addEventListener("click", () => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClear.addEventListener("click", () => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+deleteButton.addEventListener("click", () => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function createRows(r,c, array){
     const rows = document.createElement("div")
@@ -77,24 +169,25 @@ function onClick(e){
     console.log(e.textContent)
 }
 
-function  createTopRow(){
-    const keyPad = document.getElementById("key-pad");
-    const topRowArray =["C","÷","x","<"]
-    const rows = createRows(1,4,topRowArray)
-    rows.className= "top-row";
-    var content = rows.outerHTML;
-    keyPad.insertAdjacentHTML("afterbegin",content);
-}
+// function  createTopRow(){
+//     const keyPad = document.getElementById("key-pad");
+//     const topRowArray =["C","÷","x","<"]
+//     const rows = createRows(1,4,topRowArray)
+//     rows.className= "top-row";
+//     console.log(rows)
+//     var content = rows.outerHTML;
+//     keyPad.insertAdjacentHTML("afterbegin",content);
+// }
 
-function createOuterColumn(){
-    const keyPad = document.getElementById("key-pad");
-    const outerColumnArray = ["-","+","="];
-    const column = createRows(3,1, outerColumnArray);
-    column.className = "outer-column";
-    var content = column.outerHTML ; 
-    keyPad.insertAdjacentHTML("afterbegin",content);
+// function createOuterColumn(){
+//     const keyPad = document.getElementById("key-pad");
+//     const outerColumnArray = ["-","+","="];
+//     const column = createRows(3,1, outerColumnArray);
+//     column.className = "outer-column";
+//     var content = column.outerHTML ; 
+//     keyPad.insertAdjacentHTML("afterbegin",content);
 
-}
+// }
 
 // function displayState(){
 //     const displayState = document.getElementById("display-state");
@@ -104,9 +197,9 @@ function createOuterColumn(){
 // }
 
 // displayState()
-createTopRow()
+// createTopRow()
 createNumberPad()
-createOuterColumn()
+// createOuterColumn()
 
 
 // var keyPad = document.getElementById("key-pad");
